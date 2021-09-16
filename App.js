30d637d0fd25902fccbf1777e2db77431ce324a0
Petr Knetl <456302@mail.muni.cz>
2021-09-16 09:09:24 +0200

    android build

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { BleManager } from 'react-native-ble-plx';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
+  const manager = new BleManager();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Openup Appa.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
