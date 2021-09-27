fbe6969f0df13f67afc53f03e641a9e7645a6399
Petr Knetl <456302@mail.muni.cz>
2021-09-27 12:34:20 +0200

    App.js API structure

--- App.js
+++ App.js
@@ -1,17 +1,75 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
+import React, { useReducer, useEffect, useState } from 'react';
 import { BleManager, LogLevel } from 'react-native-ble-plx';
 import { StyleSheet, Text, View } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+
 import IntroScreen from "./screens/IntroScreen";
+import ConnectionsScreen from "./screens/ConnectionsScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import { SafeAreaView } from 'react-native';
 
 
 export default function App() {
-  const Stack = createNativeStackNavigator();
+  const Stack = createStackNavigator();
   const [manager, setManager] = useState(null);
 
+  const [device, setDevice] = useState(null);
+
+
+  const modeReducer = (mode, action) => {
+    switch (action) {
+      case 'SET_ON':
+        // TODO: YOUR CODE TO SET LIGH ON
+        return 'ON'
+      case 'SET_OFF':
+        // TODO: CODE TO SET LIGH OFF
+        return 'OFF'
+      case 'set_FLASH':
+        // TODO: CODE TO SET LIGH FLASHING
+        return 'FLASH'
+      default:
+        throw new Error("Unsupported modeReducer action received.")
+    }
+  }
+
+  const [mode, dispatchMode] = useReducer(modeReducer, "OFF");
+
+  /**
+   * Returns all available BLE devices.
+   *
+   * @return {array}
+   * @public
+   */
+  const getAvailableDevices = () => {
+    //TODO
+
+    // return array of devices with all their attribuse you wanna show in UI
+  }
+
+  /**
+   * Establishes connection with BLE device.
+   *
+   * @param {string} device
+   */
+  const connectToDevice = (device) => {
+
+    //TODO
+    //use setDevice(DEVICE) here - DEVICE is dict with all necessary attributes
+  }
+
+  /**
+   * Close the BLE device connection.
+   *
+   * @param {string} device
+   */
+  const disconnectDevice = () => {
+
+    //TODO
+    //use setDevice(null) here
+  }
 
   const BLE_DEVICE_NAME = "BlueIoToy";
 
@@ -56,17 +114,20 @@ export default function App() {
   }
 
   return (
-    // <View style={styles.container}>
-    //   <Text>Openupa Appa.js to start working on your app!!</Text>
+    <View style={styles.container}>
+      <Text>Openupa Appa.js to start working on your app!!</Text>
+      <StatusBar style="auto" />
+    </View>
+    // <>
+    //   <NavigationContainer>
+    //     <Stack.Navigator screenOptions={{ headerShown: false }}>
+    //       <Stack.Screen name="Intro" component={IntroScreen} />
+    //       <Stack.Screen name="Connections" component={ConnectionsScreen} />
+    //       <Stack.Screen name="Settings" component={SettingsScreen} mode={mode} dispatchMode={dispatchMode} />
+    //     </Stack.Navigator>
+    //   </NavigationContainer>
     //   <StatusBar style="auto" />
-    // </View>
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Intro" component={IntroScreen} />
-        {/* <Stack.Screen name="Connections" component={C} />
-      <Stack.Screen name="Settings" component={SettingsScreen} /> */}
-      </Stack.Navigator>
-    </NavigationContainer>
+    // </>
   );
 }
 
