416525cb44798bbc93358d996357934049e14bf2
Ladislav Štefka <stefka885@gmail.com>
2021-10-25 12:07:47 +0100

    Android icon fix, permissions fix

--- services/PermissionsService.js
+++ services/PermissionsService.js
@@ -9,7 +9,7 @@ export async function requestLocationPermission() {
 	if (Platform.OS === "android" && Platform.Version >= 23) {
     try {
       const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
       ); 
       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
         console.log('(PermissionService): Location permission for bluetooth scanning granted');
